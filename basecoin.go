d9ade665b0202a25e5463de0c1a4dec281089a10
BJ Miller <bj.miller@coinninja.com>
2019-11-19 14:19:38 -0500

    Add basecoin and initial hd wallet init and signing key funcs

---
+++ basecoin.go
@@ -0,0 +1,51 @@
+package cnlib
+
+/// Type Declaration
+
+// Basecoin is used to provide information about the current user's wallet.
+type Basecoin struct {
+	Purpose int
+	Coin    int
+	Account int
+}
+
+/// Constructors
+
+// NewBaseCoin instantiates a new object and sets values
+func NewBaseCoin(purpose int, coin int, account int) *Basecoin {
+	bc := Basecoin{Purpose: purpose, Coin: coin, Account: account}
+	return &bc
+}
+
+/// Receiver methods
+
+// UpdatePurpose updates the purpose value on the BaseCoin receiver.
+func (bc *Basecoin) UpdatePurpose(purpose int) {
+	bc.Purpose = purpose
+}
+
+// UpdateCoin updates the coin value on the BaseCoin receiver.
+func (bc *Basecoin) UpdateCoin(coin int) {
+	bc.Coin = coin
+}
+
+// UpdateAccount updates the account value on the BaseCoin receiver.
+func (bc *Basecoin) UpdateAccount(account int) {
+	bc.Account = account
+}
+
+// GetBech32HRP returns a Bech32 HRP string derived from Purpose and Coin
+func (bc *Basecoin) GetBech32HRP() string {
+	if bc == nil {
+		return ""
+	}
+
+	basecoin := *bc
+	if basecoin.Purpose != 84 {
+		return ""
+	}
+	if basecoin.Coin == 0 {
+		return "bc"
+	}
+	return "bcrt"
+}
---
+++ hdwallet.go
@@ -0,0 +1,74 @@
+package cnlib
+
+import (
+	"strings"
+
+	"github.com/tyler-smith/go-bip32"
+	"github.com/tyler-smith/go-bip39"
+	"github.com/tyler-smith/go-bip39/wordlists"
+)
+
+/// Type Declarations
+
+// HDWallet represents the user's current wallet.
+type HDWallet struct {
+	Basecoin         Basecoin
+	WalletWords      string // space-separated string of user's recovery words
+	masterPrivateKey *bip32.Key
+}
+
+// GetFullBIP39WordListString returns all 2,048 BIP39 mnemonic words as a space-separated string.
+func GetFullBIP39WordListString() string {
+	return strings.Join(wordlists.English, " ")
+}
+
+// NewWordListFromEntropy returns a space-separated list of mnemonic words from entropy.
+func NewWordListFromEntropy(entropy []byte) string {
+	mnemonic, _ := bip39.NewMnemonic(entropy)
+	return mnemonic
+}
+
+// NewHDWalletFromWords returns a pointer to an HDWallet, containing the Basecoin, words, and unexported master private key.
+func NewHDWalletFromWords(wordString string, basecoin *Basecoin) *HDWallet {
+	masterKey, err := masterPrivateKey(wordString)
+	if err != nil {
+		return nil
+	}
+	wallet := HDWallet{Basecoin: *basecoin, WalletWords: wordString, masterPrivateKey: masterKey}
+	return &wallet
+}
+
+/// Receiver functions
+
+// SigningKey returns the private key at the m/42 path.
+func (wallet *HDWallet) SigningKey() []byte {
+	return wallet.signingMasterKey().Key
+}
+
+// SigningPublicKey returns the public key at the m/42 path.
+func (wallet *HDWallet) SigningPublicKey() []byte {
+	return wallet.signingMasterKey().PublicKey().Key
+}
+
+/// Unexported functions
+
+func masterPrivateKey(wordString string) (*bip32.Key, error) {
+	seed := bip39.NewSeed(wordString, "")
+	masterKey, err := bip32.NewMasterKey(seed)
+	if err != nil {
+		return nil, err
+	}
+	return masterKey, nil
+}
+
+func (wallet *HDWallet) signingMasterKey() *bip32.Key {
+	masterKey := wallet.masterPrivateKey
+	if masterKey == nil {
+		return nil
+	}
+	childKey, childErr := masterKey.NewChildKey(42)
+	if childErr != nil {
+		return nil
+	}
+	return childKey
+}

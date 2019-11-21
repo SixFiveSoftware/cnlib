98f9f8de7b3e0c173536d1a06338848097cdedd1
BJ Miller <bj.miller@coinninja.com>
2019-11-21 11:02:49 -0500

    Expose DerivationPath init for mobile

--- derivationpath.go
+++ derivationpath.go
@@ -1,9 +1,26 @@
 package cnlib
 
+/// Type Declaration
+
+// DerivationPath is used to provide information about an address to be generated.
 type DerivationPath struct {
 	Purpose int
 	Coin    int
 	Account int
 	Change  int
 	Index   int
+}
+
+/// Constructors
+
+// NewDerivationPath instantiates a new object and sets values.
+func NewDerivationPath(purpose int, coin int, account int, change int, index int) *DerivationPath {
+	dp := DerivationPath{
+		Purpose: purpose,
+		Coin:    coin,
+		Account: account,
+		Change:  change,
+		Index:   index,
+	}
+	return &dp
 }
